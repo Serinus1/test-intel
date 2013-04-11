2ee77404eeddf82ccd242e55842721c768ce482a
Jonathan McGee <mcgee@umd.edu>
2013-04-10 22:29:28 -0400

    * Initial import of intelligence reporter

---
+++ TestIntelReporter/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestIntelReporter")]
+[assembly: AssemblyDescription("TEST Intelligence Reporting Tool")]
+[assembly: AssemblyCompany("TEST Alliance Please Ignore")]
+[assembly: AssemblyProduct("TEST Alliance Tools")]
+[assembly: AssemblyCopyright("Copyright © 2013 Something-or-other")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("0.1.0.0")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: ComVisible(false)]
