b43d63dcdd1bda043ba7342fc0fc15aa645c1b45
Jonathan McGee <mcgee@umd.edu>
2013-04-13 00:17:28 -0400

    * Auto-Run setting now works
    * Status updates now populate before the window is shown

--- TestIntelReporter/MainForm.cs
+++ TestIntelReporter/MainForm.cs
@@ -1,25 +1,27 @@
-﻿using System;
-using System.Linq;
-using System.Reflection;
+﻿using Microsoft.Win32;
+using System;
+using System.Security;
 using System.Windows.Forms;
 
 namespace TestIntelReporter {
     public partial class MainForm : Form {
         Properties.Settings settings;
         bool closing;
+        private const string AutoRunKey = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run";
+        private const string AutoRunValue = "TestIntelReporter";
 
         public MainForm() {
             InitializeComponent();
 
             // Initialize additional properties
             this.Icon = Properties.Resources.AppIcon;
-            this.settings = Properties.Settings.Default;
             labelAppName.Text = String.Format(labelAppVersion.Text, Application.ProductName);
             labelAppVersion.Text = String.Format(labelAppVersion.Text, Application.ProductVersion);
             notifyIcon.Icon = Properties.Resources.AppIcon;
             notifyIcon.Visible = true;
 
             // Load the Configuration
+            this.settings = Properties.Settings.Default;
             textUsername.Text = logWatcher.Username = settings.Username;
             if (!String.IsNullOrEmpty(settings.PasswordHash)) {
                 logWatcher.PasswordHash = settings.PasswordHash;
@@ -31,11 +33,57 @@ namespace TestIntelReporter {
             checkAutostart.Checked = settings.AutoStart;
             checkMinimizeOnClose.Checked = settings.HideOnClose;
             checkHideMinimized.Checked = settings.HideOnMinimize;
+            this.UpdateAutoRun();
 
             // Update the status
             if (!String.IsNullOrEmpty(settings.Username)
                     && !String.IsNullOrEmpty(settings.PasswordHash)) {
                 logWatcher.Start();
+            }
+            this.UpdateStatus();
+        }
+
+        private void UpdateAutoRun() {
+            try {
+                using (var key = Registry.CurrentUser.OpenSubKey(AutoRunKey, true)) {
+                    if (key != null) {
+                        if (settings.AutoStart) {
+                            key.SetValue(AutoRunValue,
+                                '"' + Application.ExecutablePath + '"',
+                                RegistryValueKind.String);
+                        } else {
+                            key.DeleteValue(AutoRunValue, false);
+                        }
+                    }
+                }
+            } catch(SecurityException) {
+            }
+        }
+
+        private void UpdateStatus() {
+            toolStripStatusIntel.Text = string.Format(
+                (logWatcher.IntelReported == 1)
+                    ? Properties.Resources.OneIntelReported
+                    : Properties.Resources.MultiIntelReported,
+                logWatcher.IntelReported);
+            toolStripStatusUsers.Text = string.Format(
+                (logWatcher.Users == 1)
+                    ? Properties.Resources.OneUserReporting
+                    : Properties.Resources.MultiUserReporting,
+                logWatcher.Users);
+
+            if (logWatcher.IsConnected) {
+                toolStripStatus.Text = Properties.Resources.AppConnected;
+                toolStripStatusUsers.Visible = true;
+            } else if (logWatcher.BadPassword) {
+                toolStripStatus.Text = Properties.Resources.AppAuthenticateFailed;
+                toolStripStatusUsers.Visible = false;
+            } else if (logWatcher.IsRunning) {
+                toolStripStatus.Text = Properties.Resources.AppIdle;
+                toolStripStatusUsers.Visible = false;
+            } else {
+                toolStripStatus.Text = Properties.Resources.AppConfigure;
+                toolStripStatusUsers.Visible = false;
             }
         }
 
@@ -83,6 +131,7 @@ namespace TestIntelReporter {
         private void checkAutostart_CheckedChanged(object sender, EventArgs e) {
             settings.AutoStart = checkAutostart.Checked;
             settings.Save();
+            UpdateAutoRun();
         }
 
         private void linkTestMap_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e) {
@@ -144,35 +193,12 @@ namespace TestIntelReporter {
         }
 
         private void logWatcher_StatusChanged(object sender, EventArgs e) {
-            if (!this.IsHandleCreated)
-                return;
-
-            this.BeginInvoke((MethodInvoker) delegate {
-                toolStripStatusIntel.Text = string.Format(
-                    (logWatcher.IntelReported == 1)
-                        ? Properties.Resources.OneIntelReported
-                        : Properties.Resources.MultiIntelReported,
-                    logWatcher.IntelReported);
-                toolStripStatusUsers.Text = string.Format(
-                    (logWatcher.Users == 1)
-                        ? Properties.Resources.OneUserReporting
-                        : Properties.Resources.MultiUserReporting,
-                    logWatcher.Users);
-
-                if (logWatcher.IsConnected) {
-                    toolStripStatus.Text = Properties.Resources.AppConnected;
-                    toolStripStatusUsers.Visible = true;
-                } else if (logWatcher.BadPassword) {
-                    toolStripStatus.Text = Properties.Resources.AppAuthenticateFailed;
-                    toolStripStatusUsers.Visible = false;
-                } else if (logWatcher.IsRunning) {
-                    toolStripStatus.Text = Properties.Resources.AppIdle;
-                    toolStripStatusUsers.Visible = false;
-                } else {
-                    toolStripStatus.Text = Properties.Resources.AppConfigure;
-                    toolStripStatusUsers.Visible = false;
-                }
-            });
+            if (!this.IsHandleCreated) {
+                this.UpdateStatus();
+            } else {
+                this.BeginInvoke((MethodInvoker)this.UpdateStatus, null);
+            }
         }
+
     }
 }
