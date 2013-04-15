dd191b49a8c48d4fea7ac089bf95f88c2701397e
Jonathan McGee <mcgee@umd.edu>
2013-04-15 01:28:55 -0400

    * Some more revisions to the intel reporting component.

--- PleaseIgnore.IntelMap/IntelReporter.cs
+++ PleaseIgnore.IntelMap/IntelReporter.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics.Contracts;
 using System.IO;
+using System.Net;
+using System.Security.Authentication;
 using System.Threading;
 
 namespace PleaseIgnore.IntelMap {
@@ -19,6 +21,8 @@ namespace PleaseIgnore.IntelMap {
         private volatile bool authenticate;
         // Signals the worker thread to process a signal
         private AutoResetEvent signal;
+        // Synchronization object
+        private object syncObject;
 
         // Thread used for enternal monitoring
         private Thread thread;
@@ -26,6 +30,10 @@ namespace PleaseIgnore.IntelMap {
         private List<IntelChannel> channels;
         // File system event queue
         private ConcurrentQueue<FileSystemEventArgs> watcherEvents;
+        // The intel reporting server session
+        private IntelSession session;
+        // The last time we failed to contact the server
+        private DateTime lastFailure;
 
         // The overridden file directory for EVE logs
         private volatile string logDirectory;
@@ -48,9 +56,11 @@ namespace PleaseIgnore.IntelMap {
         public IntelReporter() {
             this.signal = new AutoResetEvent(false);
             this.watcherEvents = new ConcurrentQueue<FileSystemEventArgs>();
+            this.syncObject = new object();
 
             this.ChannelUpdatePeriod = new TimeSpan(24, 0, 0);
             this.ChannelScanFrequency = new TimeSpan(0, 0, 5);
+            this.RetryPeriod = new TimeSpan(0, 10, 0);
             this.username = String.Empty;
             this.password = String.Empty;
         }
@@ -97,6 +107,13 @@ namespace PleaseIgnore.IntelMap {
         /// </summary>
         [DefaultValue(typeof(TimeSpan), "00:00:05"), Category("Behavior")]
         public TimeSpan ChannelScanFrequency { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the time to back off attempting to contact the server
+        ///     again if networking problems prevent communication.
+        /// </summary>
+        [DefaultValue(typeof(TimeSpan), "00:10:00"), Category("Behavior")]
+        public TimeSpan RetryPeriod { get; set; }
 
         /// <summary>
         ///     Gets or sets the TEST Alliance AUTH username.
@@ -171,6 +188,19 @@ namespace PleaseIgnore.IntelMap {
         public int IntelSent { get; private set; }
 
         /// <summary>
+        ///     Gets the number of events that were dropped because of network
+        ///     or server errors.
+        /// </summary>
+        [Browsable(false)]
+        public int IntelDropped { get; private set; }
+
+        /// <summary>
+        ///     Gets the current state of the intel reporting engine.
+        /// </summary>
+        [Browsable(false)]
+        public IntelStatus Status { get; private set;  }
+
+        /// <summary>
         ///     Raised when a new intel report has been parsed from a log file.
         /// </summary>
         /// <remarks>
@@ -200,7 +230,7 @@ namespace PleaseIgnore.IntelMap {
 
         public void Start() {
             Contract.Ensures(this.Enabled == true);
-            lock (this) {
+            lock (this.syncObject) {
                 this.running = true;
                 if (!this.initializing && !this.DesignMode
                         && (this.thread == null)) {
@@ -211,7 +241,7 @@ namespace PleaseIgnore.IntelMap {
 
         public void Stop() {
             Contract.Ensures(this.Enabled == false);
-            lock (this) {
+            lock (this.syncObject) {
                 this.running = false;
                 if (this.thread != null) {
                     this.thread.Join();
@@ -241,65 +271,176 @@ namespace PleaseIgnore.IntelMap {
         ///     Entry point for the 
         /// </summary>
         private void ThreadMain() {
-            signal.Reset();
-            using (var container = new Container()) {
-                // Configure the File Watcher
-                IntelSession session = null;
-                var watcher = new FileSystemWatcher();
-                watcher.BeginInit();
-                watcher.Path = this.LogDirectory;
-                watcher.Filter = "*.txt";
-                watcher.Created += watcher_Created;
-                watcher.Changed += watcher_Changed;
-                watcher.EnableRaisingEvents = true;
-                container.Add(watcher);
-                watcher.EndInit();
+            try {
+                this.Status = IntelStatus.Idle;
+                signal.Reset();
+                using (var container = new Container()) {
+                    // Configure the File Watcher
+                    var watcher = new FileSystemWatcher();
+                    watcher.BeginInit();
+                    // TODO: Fails if the path doesn't exist
+                    watcher.Path = this.LogDirectory;
+                    watcher.Filter = "*.txt";
+                    watcher.Created += watcher_Created;
+                    watcher.Changed += watcher_Changed;
+                    watcher.NotifyFilter = NotifyFilters.LastWrite
+                        | NotifyFilters.Size;
+                    container.Add(watcher);
+                    watcher.EnableRaisingEvents = true;
+                    watcher.EndInit();
 
-                while (this.running) {
-                    // Start scanning from a different log directory
-                    if (resetDirectory) {
-                        resetDirectory = false;
-                        watcher.Path = this.LogDirectory;
-                        // TODO: Reset children
-                    }
-
-                    // Change the login credentials
-                    if (authenticate) {
-                        if (session != null) {
-                            session.Dispose();
-                            session = null;
+                    // The main loop
+                    while (this.running) {
+                        // Start scanning from a different log directory
+                        if (resetDirectory) {
+                            resetDirectory = false;
+                            watcher.Path = this.LogDirectory;
+                            // TODO: Reset children
                         }
+
+                        // Change the login credentials
+                        if (authenticate) {
+                            authenticate = false;
+                            if (this.Status == IntelStatus.AuthenticationFailure) {
+                                // TODO: Might want a different state
+                                this.Status = IntelStatus.Idle;
+                            }
+                            if (session != null) {
+                                session.Dispose();
+                                session = null;
+                            }
+                            CreateSession();
+                        }
+
+                        // We've been notified of a directory change
+                        FileSystemEventArgs args;
+                        while (watcherEvents.TryDequeue(out args)) {
+                            // TODO: Notify children
+                        }
+
+                        // TODO: Have all children rescan their log files
+
+                        // Wait for another event
+                        signal.WaitOne(this.ChannelScanFrequency);
                     }
 
-                    // We've been notified of a directory change
-                    FileSystemEventArgs args;
-                    while(watcherEvents.TryDequeue(out args)) {
-                        // TODO: Notify children
+                    // Terminate the session before disconnecting
+                    if (this.session != null) {
+                        session.Dispose();
+                        this.session = null;
                     }
-
-                    // Have all children rescan their log files
-
-                    // Wait for another event
-                    signal.WaitOne(this.ChannelScanFrequency);
-                }
-
-                if (session != null) {
-                    session.Dispose();
-                }
-            } //using (var container = new Container()) {
+                } //using (var container = new Container()) {
+                this.Status = IntelStatus.Stopped;
+            } catch {
+                this.Status = IntelStatus.FatalError;
+            }
         }
 
         /// <summary>
         ///     Receives Intel reports from the child channel listeners.
         /// </summary>
-        internal void OnIntelReported(IntelEventArgs e) {
+        /// <returns>
+        ///     <see langword="true"/> if the intel was sucessfully sent to the
+        ///     reporting server; otherwise, <see langword="false"/>.
+        /// </returns>
+        internal bool OnIntelReported(IntelEventArgs e) {
             Contract.Requires<ArgumentNullException>(e != null, "e");
-            ++this.IntelSent;
 
+            // Send into the ThreadPool so not to interfere with our processing
             var handler = this.IntelReported;
             if (handler != null) {
-                handler(this, e);
+                ThreadPool.QueueUserWorkItem(args => handler(this, (IntelEventArgs)args), e);
             }
+
+            // See if it's worth trying again
+            switch (this.Status) {
+            case IntelStatus.AuthenticationFailure:
+                ++this.IntelDropped;
+                return false;
+            case IntelStatus.NetworkFailure:
+            case IntelStatus.ServerFailure:
+                var now = DateTime.UtcNow;
+                if (now - lastFailure < this.RetryPeriod) {
+                    ++this.IntelDropped;
+                    return false;
+                }
+                break;
+            }
+
+            // First, try an already existing session
+            if (this.session != null) {
+                if (this.SendReport(e, false)) {
+                    // Success
+                    return true;
+                } else if (this.session != null) {
+                    // If the session object remains, it wasn't a session problem
+                    return false;
+                }
+            }
+
+            // Try again after (re)openning the session
+            this.CreateSession();
+            return this.SendReport(e, true);
+        }
+
+        private bool CreateSession() {
+            Contract.Requires(this.session == null);
+            Contract.Ensures(this.session != null);
+
+            // Once authentication fails, don't try again until the user fixes it
+            if (this.Status == IntelStatus.AuthenticationFailure) {
+                return false;
+            }
+
+            // Try openning a new server connection
+            try {
+                this.session = new IntelSession(this.username, this.password);
+                return true;
+            } catch (AuthenticationException) {
+                // Will not try again until the user corrects this
+                this.Status = IntelStatus.AuthenticationFailure;
+            } catch (WebException) {
+                // Network problems, try again later.
+                this.Status = IntelStatus.NetworkFailure;
+            } catch (IntelException) {
+                // The server did something...odd.
+                this.Status = IntelStatus.ServerFailure;
+            }
+
+            this.lastFailure = DateTime.UtcNow;
+            return false;
+        }
+
+        private bool SendReport(IntelEventArgs e, bool lastTry) {
+            // Require a session to send the attempt
+            if (this.session == null) {
+                this.IntelSent += lastTry ? 1 : 0;
+                return false;
+            }
+
+            // Catch the expected errors and update the status appropriately
+            try {
+                if (this.session.Report(e.Channel.Name, e.Timestamp, e.Message)) {
+                    // Successfully sent
+                    ++this.IntelSent;
+                    return true;
+                } else {
+                    // Session expired
+                    this.session = null;
+                    this.IntelSent += lastTry ? 1 : 0;
+                    return false;
+                }
+            } catch (WebException) {
+                // Dropping due to network problems
+                this.Status = IntelStatus.NetworkFailure;
+            } catch (IntelException) {
+                // The server did something...odd.
+                this.Status = IntelStatus.ServerFailure;
+            }
+            
+            ++this.IntelDropped;
+            this.lastFailure = DateTime.UtcNow;
+            return false;
         }
 
         /// <summary>
@@ -322,7 +463,9 @@ namespace PleaseIgnore.IntelMap {
         private void ObjectInvariant() {
             Contract.Invariant(this.ChannelUpdatePeriod > TimeSpan.Zero);
             Contract.Invariant(this.ChannelScanFrequency > TimeSpan.Zero);
+            Contract.Invariant(this.RetryPeriod > TimeSpan.Zero);
             Contract.Invariant(this.IntelSent >= 0);
+            Contract.Invariant(this.IntelDropped >= 0);
         }
     }
 }
---
+++ PleaseIgnore.IntelMap/IntelStatus.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PleaseIgnore.IntelMap {
+    public enum IntelStatus {
+        /// <summary>
+        ///     The <see cref="IntelReporter"/> is not currently running.
+        /// </summary>
+        Stopped,
+        /// <summary>
+        ///     The <see cref="IntelReporter"/> is waiting for EVE to start
+        ///     logging intel.
+        /// </summary>
+        Idle,
+        /// <summary>
+        ///     The <see cref="IntelReporter"/> is reporting Intel to the
+        ///     server.
+        /// </summary>
+        Running,
+        /// <summary>
+        ///     The <see cref="IntelReporter.Username"/> or
+        ///     <see cref="IntelReporter.PasswordHash"/> are incorrect.
+        /// </summary>
+        AuthenticationFailure,
+        /// <summary>
+        ///     The last attempt to contact the server failed due to network
+        ///     problems.
+        /// </summary>
+        NetworkFailure,
+        /// <summary>
+        ///     The last attempt to contact the server failed due to an expected
+        ///     server response.
+        /// </summary>
+        ServerFailure,
+        /// <summary>
+        ///     An unknown problem forced the service to terminate.
+        /// </summary>
+        FatalError
+    }
+}
